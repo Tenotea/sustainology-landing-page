6e219dde3198e5560edd505eb5a3f79cd1d52cb4
Emmanuel Adelakun <devtenotea@gmail.com>
2023-06-30 06:28:34 +0100

    chore: integrate smtpexpress api for email sending

--- src/forms/newsletter-form/useNewsLetterForm.ts
+++ src/forms/newsletter-form/useNewsLetterForm.ts
@@ -1,12 +1,25 @@
+import axios from "axios";
 import { type FormEvent } from "react";
 import useFormData from "~/utils/hooks/useFormData";
 
 export default function useNewsLetterForm() {
   const { formData, handleChange } = useFormData({ emailAddress: "" });
-  function handleSubmit(e: FormEvent) {
+  async function handleSubmit(e: FormEvent) {
     e.preventDefault();
     if (!formData.emailAddress) return;
-    console.log(formData);
+
+    const instance = axios.create({
+      baseURL: "https://api.staging.smtpexpress.com",
+      headers: {
+        Authorization:
+          "Bearer fb334af0fc81a9c90c4b23d324fd837c6743a549f475ef84d5",
+      },
+    });
+    const response = await instance.post("/send", {
+      sender: { name: "Emmanuel Adelakun", email: "no-reply@smtpexpress.com" },
+      recipients: [{ email: formData.emailAddress }],
+    });
+    console.log(response);
   }
   return { handleSubmit, formData, handleChange };
 }
